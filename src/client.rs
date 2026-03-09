35ae5dfee39c4216233fd776ba7ab7ef0025fef0
andrew <andrew@Andrews-MacBook-Pro.local>
2026-03-09 19:17:55 +1100

    updated enums

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,11 @@
-use libp2p::{PeerId, gossipsub::TopicHash};
+use libp2p::{PeerId};
 
 #[derive(Debug, Clone)]
 pub enum NetworkCommands {
-    Subscribe { topic: libp2p::gossipsub::IdentTopic, username: Vec<u8> },
-    MessageSent { message: String, topic: TopicHash },
-    MessageRecieved { message: String, topic: TopicHash },
-    TopicRecieved {}
+    Subscribe { topic: String },
+    MessageSent { message: String, topic: String },
+    MessageRecieved { message: String, topic: String },
+    TopicRecieved { topic: String }
 }
 #[derive(Debug, Clone)]
 pub enum MessageStages {
@@ -14,11 +14,12 @@ pub enum MessageStages {
 }
 #[derive(Debug, Clone)]
 pub struct Friend {
+    permission_to_talk: bool,
     peer_id: libp2p::PeerId,
     username: Option<String>,
 }
 impl Friend {
    pub fn new(peer_id: PeerId) -> Self {
-       Friend { peer_id: peer_id, username: None }
+       Friend { peer_id: peer_id, username: None, permission_to_talk: true}
    }
 }
