de0b6d9edcd91bb1fd38d22dd18dbf9253cab2e5
andrew <andrew@Andrews-MacBook-Pro.local>
2026-03-09 19:16:12 +1100

    testing for full implmentation

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
+use libp2p::gossipsub::Message;
 use tokio::sync::mpsc;
 use crate::{client::NetworkCommands, network::User};
 mod client;
 mod ui;
 mod network;
-#[tokio::main]
+use tokio::io::{AsyncBufReadExt, BufReader};
+
+#[tokio::main(flavor = "multi_thread")]
 async fn main() {
-    let (sender_to_nw, reciever_to_nw) = mpsc::channel::<NetworkCommands>(100);
-    let ui_handle = tokio::spawn(async move {
-        let ui = ui::run_ui();
+    let (sender_to_nw, reciever_to_nw) = mpsc::channel(100);
+    let (sender_to_ui, reciever_to_ui) = mpsc::channel(100);
+    let mut new_user = User::new(sender_to_ui.clone(), reciever_to_nw).await.unwrap();
+
+    let value = sender_to_nw.clone();
+    let handler_ui = tokio::task::spawn_blocking(move || {
+        // remember to change this after UI designing
+        let _  = ui::run_ui(reciever_to_ui, value).unwrap();
     });
     let handle = tokio::task::spawn(async move {
-        let mut new_user = User::new(sender_to_nw, reciever_to_nw).await.unwrap();
-        for i in new_user.listing_all_people() {
-            log::info!("This new person is: {:?}", i);
-        }
         let _ = new_user.run().await.unwrap();
-        sender_to_nw.clone().send(NetworkCommands::MessageSent { message: "Hello world".to_string(), topic: new_user.listing_all_people()[0].clone() });
-        
     });
-    let asld = handle.await;
-    let asd = ui_handle.await;
-}
 
+    sender_to_nw.send(
+        NetworkCommands::Subscribe { topic: "This is how this looks like".to_string() }
+    ).await.unwrap();
+    sender_to_nw.send(
+        NetworkCommands::MessageSent { message: "Hello world".to_string(), topic: "Hello world".to_string() }
+    ).await.unwrap();
+
+    handler_ui.await;
+    let _ = handle.await;
+}
